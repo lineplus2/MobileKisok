a7c33917e709a23e5826fcc44ddd81a1997d2009
deok il <duckdi97@gmail.com>
2021-03-18 21:27:37 +0900

    signup

--- app/src/main/java/com/example/kw_mk/Signup.java
+++ app/src/main/java/com/example/kw_mk/Signup.java
@@ -4,10 +4,16 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 
-public class SignupActivity extends AppCompatActivity {
+
+public class Signup extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.singup);
+
     }
+
+
+
 }

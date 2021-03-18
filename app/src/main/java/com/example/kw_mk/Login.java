7a1713c0bf3a9262287aa15d8ff4f12a4af8415d
username <user@email.mail>
2021-03-18 20:03:53 +0900

    2021.03.18 20:03

--- app/src/main/java/com/example/kw_mk/Login.java
+++ app/src/main/java/com/example/kw_mk/Login.java
@@ -6,30 +6,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-
-import com.google.firebase.database.DatabaseReference;
-
-import org.w3c.dom.Text;
 
 public class Login extends AppCompatActivity {
-
-    private Button btn_signup;
-
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
-        btn_signup = (Button) findViewById(R.id.btn_signup);
-        btn_signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Login.this, Signup.class);
-                startActivity(intent);
-            }
-        });
-
+        findViewById(R.id.btn_signup).setOnClickListener(onClickListener);
     }
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.btn_login:
+                    break;
+
+                case R.id.btn_signup:
+                    intent = new Intent(Login.this, Signup.class);
+                    startActivity(intent);
+                    break;
+            }
+        }
+    };
+
+
 }
--- app/src/main/java/com/example/kw_mk/Signup.java
+++ app/src/main/java/com/example/kw_mk/Signup.java
@@ -4,16 +4,10 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 
-
 public class Signup extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.singup);
-
     }
-
-
-
 }

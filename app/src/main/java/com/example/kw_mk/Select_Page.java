2328c43a009091738cf8826c46572ceb7cd21b34
deok il <duckdi97@gmail.com>
2021-03-25 05:33:20 +0900

    layout

---
+++ app/src/main/java/com/example/kw_mk/Select_Page.java
@@ -0,0 +1,39 @@
+package com.example.kw_mk;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Select_Page extends AppCompatActivity {
+
+    private Button btn_order;
+    private Button btn_customer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.select_page);
+
+        btn_order = (Button) findViewById(R.id.btn_order);
+        btn_order.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Select_Page.this, Order_Main.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_customer = (Button) findViewById(R.id.btn_customer);
+        btn_customer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+
+}

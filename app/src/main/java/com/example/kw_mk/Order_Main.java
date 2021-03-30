50448974f663572775c39d8b17e63ede9018a871
duckgus <duckdi97@gmail.com>
2021-03-30 15:34:00 +0900

    UI

--- app/src/main/java/com/example/kw_mk/Order_Main.java
+++ app/src/main/java/com/example/kw_mk/Order_Main.java
@@ -3,13 +3,20 @@ package com.example.kw_mk;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 public class Order_Main extends AppCompatActivity {
+
+    private FragmentManager fragmentManager = getSupportFragmentManager();
+
+    private Order_Main_Storemgmt order_main_storemgmt = new Order_Main_Storemgmt();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.order_main);
+
+
 
     }
 }

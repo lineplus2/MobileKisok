eead0a5222583e2f61eb128fa02eff2bf9d1afde
lineplus2 <lineplus2@naver.com>
2021-03-31 12:22:11 +0900

    Login

---
+++ app/src/main/java/com/example/kw_mk/App.java
@@ -0,0 +1,15 @@
+package com.example.kw_mk;
+
+import android.app.Application;
+
+public class App extends Application {
+
+
+    public static  String LoginUserEmail = null;
+    public static  String LoginUserPw = null;
+    public static  String LoginUserName = null;
+    public static  String LoginUserPhone = null;
+
+
+
+}
--- app/src/main/java/com/example/kw_mk/LoginActivity.java
+++ app/src/main/java/com/example/kw_mk/LoginActivity.java
@@ -3,10 +3,12 @@ package com.example.kw_mk;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,6 +18,9 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -23,6 +28,8 @@ public class LoginActivity extends AppCompatActivity {
     private Button btn_login;
     private FirebaseAuth mAuth;
     String Email, Password;
+    String TAG = "LoginActivity";
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
 
     @Override
@@ -31,24 +38,31 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         mAuth = FirebaseAuth.getInstance();
 
-        btn_signup = (Button) findViewById(R.id.btn_signup);
-        btn_signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
-                startActivity(intent);
-            }
-        });
+        btn_signup = findViewById(R.id.btn_signup);
+        btn_login = findViewById(R.id.btn_login);
 
-        btn_login = (Button) findViewById(R.id.btn_login);
-        btn_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Login();
+        btn_signup.setOnClickListener(onClickListener);
+        btn_login.setOnClickListener(onClickListener);
 
-            }
-        });
     }
+
+
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.btn_login:
+                    Login();
+                    break;
+                case R.id.btn_signup:
+                    Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                    startActivity(intent);
+                    finish();
+
+            }
+        }
+    };
 
 
     private void Login() {
@@ -56,6 +70,7 @@ public class LoginActivity extends AppCompatActivity {
         Password = ((EditText) findViewById(R.id.et_pw)).getText().toString();
 
         if (TextUtils.isEmpty(Email)) {
+
             return;
         } else if (TextUtils.isEmpty(Password)) {
             return;
@@ -66,6 +81,7 @@ public class LoginActivity extends AppCompatActivity {
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (task.isSuccessful()) {
                     Intent intent = new Intent(LoginActivity.this, Select_Page.class);
+                    LoginUserDataSet(Email);
                     startActivity(intent);
                     finish();
                 } else {
@@ -73,5 +89,28 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    private void LoginUserDataSet(String Email) {  // 로그인유저 데이터 저장
+        DocumentReference docRef = db.collection("User_Info").document(Email);
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        App.LoginUserEmail = (String)document.get("email");
+                        App.LoginUserPw = (String)document.get("pw");
+                        App.LoginUserName = (String)document.get("name");
+                        App.LoginUserPhone = (String)document.get("phone");
+                    } else {
+                        Log.d(TAG, "No such document");
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
+
     }
 }

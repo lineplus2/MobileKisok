84d01bfbc42c05b8c8c900d1915da2fc8a747ce7
lineplus2 <lineplus2@naver.com>
2021-05-18 12:55:10 +0900

    MyPageImage

--- app/src/main/java/com/example/kw_mk/App.java
+++ app/src/main/java/com/example/kw_mk/App.java
@@ -2,9 +2,12 @@ package com.example.kw_mk;
 
 import android.app.Application;
 import android.location.Location;
+import android.net.Uri;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 
 public class App extends Application {
@@ -13,8 +16,12 @@ public class App extends Application {
     public static String LoginUserPw = null;
     public static String LoginUserName = null;
     public static String LoginUserPhone = null;
+    public static Uri LoginUserUri = null;
     public static FirebaseAuth mAuth = FirebaseAuth.getInstance();
     public static FirebaseFirestore db = FirebaseFirestore.getInstance();
+    public static FirebaseStorage storage = FirebaseStorage.getInstance();
+    public static StorageReference storageRef = storage.getReference();
+
 
     public static GpsTracker gpsTracker;
 
--- app/src/main/java/com/example/kw_mk/ConsumerMainActivity.java
+++ app/src/main/java/com/example/kw_mk/ConsumerMainActivity.java
@@ -1,6 +1,7 @@
 package com.example.kw_mk;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 
@@ -10,7 +11,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.storage.StorageReference;
 
 public class ConsumerMainActivity extends AppCompatActivity {
 
@@ -22,6 +25,8 @@ public class ConsumerMainActivity extends AppCompatActivity {
 
     public static Activity AActivity;
 
+    StorageReference stoRef;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +37,8 @@ public class ConsumerMainActivity extends AppCompatActivity {
         fragmentConsumerMypage = new FragmentConsumerMypage();
 
         AActivity = ConsumerMainActivity.this;
+
+
 
         trans = fm.beginTransaction();
         trans.replace(R.id.frameLayout, fragmentConsumerHome).commitAllowingStateLoss();

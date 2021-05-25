76e0887fdc641be4dbd735eee7226edc65d6ed87
duckgus <duckdi97@gmail.com>
2021-05-25 20:19:50 +0900

    consumer_main_store

--- app/src/main/java/com/example/kw_mk/Select_Page.java
+++ app/src/main/java/com/example/kw_mk/Select_Page.java
@@ -1,15 +1,21 @@
 package com.example.kw_mk;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
@@ -26,7 +32,9 @@ public class Select_Page extends AppCompatActivity {
 
     private Button btn_order;
     private Button btn_customer;
-
+    private ProgressBar loading;
+    private ConstraintLayout backcolor;
+    ProgressDialog customProgressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +43,9 @@ public class Select_Page extends AppCompatActivity {
 
         btn_order = (Button) findViewById(R.id.btn_order);
         btn_customer = (Button) findViewById(R.id.btn_customer);
+        loading = (ProgressBar) findViewById(R.id.loading);
+        backcolor = (ConstraintLayout) findViewById(R.id.background);
+
 
         // 오더용 버튼
         btn_order.setOnClickListener(new View.OnClickListener() {
@@ -42,6 +53,10 @@ public class Select_Page extends AppCompatActivity {
             public void onClick(View v) {
                 String email = FirebaseAuth.getInstance().getCurrentUser().getEmail();
                 DocumentReference docRef = FirebaseFirestore.getInstance().collection("User_Info").document(email);
+
+                loading.setVisibility(View.VISIBLE);
+                btn_order.setVisibility(View.INVISIBLE);
+                btn_customer.setVisibility(View.INVISIBLE);
 
                 docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
                     @Override
@@ -73,6 +88,9 @@ public class Select_Page extends AppCompatActivity {
         btn_customer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                loading.setVisibility(View.VISIBLE);
+                btn_order.setVisibility(View.INVISIBLE);
+                btn_customer.setVisibility(View.INVISIBLE);
                 Intent intent = new Intent(Select_Page.this, ConsumerMainActivity.class);
                 startActivity(intent);
                 finish();
